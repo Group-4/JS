dc8c41f20d22354b008f97986cbaaf5fc1b424b7
Trini Morrison <TRPMorrison@gmail.com>
2015-06-19 12:01:25 -0400

    OWNER OF PICTURES AND SOLVED ADDED TO MAIN.VIEW AND HBS

--- app/js/templates.js
+++ app/js/templates.js
@@ -47,9 +47,11 @@ this["hbs"]["main"] = Handlebars.template({"1":function(depth0,helpers,partials,
     + "\"><img src=\""
     + alias3(((helper = (helper = helpers.image_url || (depth0 != null ? depth0.image_url : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"image_url","hash":{},"data":data}) : helper)))
     + "\"></a>\n    </div>\n\n  <li data-id=\""
+    + alias3(((helper = (helper = helpers.user_id || (depth0 != null ? depth0.user_id : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"user_id","hash":{},"data":data}) : helper)))
+    + "\"> \""
     + alias3(((helper = (helper = helpers.owner || (depth0 != null ? depth0.owner : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"owner","hash":{},"data":data}) : helper)))
-    + "\"> + \""
-    + alias3(((helper = (helper = helpers.solved || (depth0 != null ? depth0.solved : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"solved","hash":{},"data":data}) : helper)))
+    + "\" \""
+    + alias3(((helper = (helper = helpers.answer || (depth0 != null ? depth0.answer : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"answer","hash":{},"data":data}) : helper)))
     + "\"\n\n\n  </li>\n";
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var stack1, helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
--- app/js/views/main.view.js
+++ app/js/views/main.view.js
@@ -24,38 +24,12 @@
 
     this.collection.fetch().done(function(data) {
 
-    this.$el.html(this.template({image: this.collection.toJSON()}))
+    this.$el.html(this.template({image: this.collection.toJSON(), owner: this.collection.toJSON(), answer: this.collection.toJSON()}))
     }.bind(this));
 
-  // TRINI ADDED THIS
-    this.collection.fetch().done(function(data) {
 
-      var picOwner = this.collection.get(this.owner);
-      this.$el.html(this.template(picOwner.toJSON()));
-    }.bind(this));
 
-    this.collection.fetch().done(function(data) {
-      var correctSolves = this.collection.get(this.solved);
-      this.$el.html(this.template(correctSolves.toJSON()));
-    }.bind(this));
-
-    },
-// DELETING ACCOUNT
-deleteAccount: function(event) {
-event.preventDefault();
-
-var button = event.target;
-var modelID = $(button).data('id');
-
-if (window.confirm("Delete your Qpic account?")) {
-    this.collection.destroy().success(function() {
-        app.mainRouter.navigate('', {
-            trigger: true
-          });
-       });
-
-      }
-    }
+}
 
   });
 }());
