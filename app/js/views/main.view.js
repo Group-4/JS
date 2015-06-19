e39cbe1353b0ae5ef55f953c572183eb6f6d8a36
Trini Morrison <TRPMorrison@gmail.com>
2015-06-19 09:28:14 -0400

    RENDER FILE UPDATED TO LINK THE OWNER AND THE NUMBER OF SOLVED/CORRECT/INCORRECT ANSWERS

--- app/js/views/main.view.js
+++ app/js/views/main.view.js
@@ -1,50 +1,59 @@
-;(function() {
+;
+(function() {
 
-  'use strict';
-
-
-  app.Views.Main = Backbone.View.extend ({
-
-    className: 'main',
-
-    events: {
-
-        'click #delete': 'deleteAccount',
-        'click #takemetosingleview' : 'singleViewFunction'
-    },
-
-    template: hbs.main,
-
-    initialize: function(options) {
-
-        var args = options || {};
-        this.collection = args.collection;
-        this.render();
-        $('.container').html(this.el);
+        'use strict';
 
 
-    },
+        app.Views.Main = Backbone.View.extend({
 
-    render: function() {
-      this.collection = new app.Collections.Posts();
+                className: 'main',
 
-      this.collection.fetch().done(function (data) {
-        this.$el.html(this.template({ image: this.collection.toJSON() }))
-      }.bind(this));
+                events: {
 
-// TRINI ADDED THIS
+                    'click #delete': 'deleteAccount',
+                    'click #takemetosingleview': 'singleViewFunction'
+                },
 
-      this.$el.html(this.template({ owner: this.collection.toJSON() }))
-    }.bind(this));
+                template: hbs.main,
 
-  this.$el.html(this.template({ solved: this.collection.toJSON() }))
-}.bind(this));
+                initialize: function(options) {
 
-    },
+                    var args = options || {};
+                    this.collection = args.collection;
+                    this.render();
+                    $('.container').html(this.el);
 
 
-    singleViewFunction: function() {
-      app.mainRouter.navigate('/single', {trigger: true});
+                },
+
+                render: function() {
+                    this.collection = new app.Collections.Posts();
+
+                    this.collection.fetch().done(function(data) {
+                        this.$el.html(this.template({
+                            image: this.collection.toJSON()
+                        }))
+                    }.bind(this));
+
+                    // TRINI ADDED THIS
+
+                    this.$el.html(this.template({
+                        owner: this.collection.toJSON()
+                    }))
+                }.bind(this));
+
+            this.$el.html(this.template({
+                solved: this.collection.toJSON()
+            }))
+        }.bind(this));
+
+},
+
+
+singleViewFunction: function() {
+        app.mainRouter.navigate('/single', {
+            trigger: true
+        });
 
     },
 
@@ -52,19 +61,21 @@
 
 
     // DELETING ACCOUNT
-    deleteAccount: function (event) {
+    deleteAccount: function(event) {
 
-      event.preventDefault();
+        event.preventDefault();
 
-      var button = event.target;
-      var modelID = $(button).data('id');
+        var button = event.target;
+        var modelID = $(button).data('id');
 
-      if (window.confirm("Delete your Qpic account?")) {
-        this.collection.destroy().success( function () {
-           app.mainRouter.navigate('', { trigger: true });
-        });
-      }
-  }
+        if (window.confirm("Delete your Qpic account?")) {
+            this.collection.destroy().success(function() {
+                app.mainRouter.navigate('', {
+                    trigger: true
+                });
+            });
+        }
+    }
 
 });
 
