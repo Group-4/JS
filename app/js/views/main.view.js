e5ddefce7f90cad0e57c5debfba8beeabbe4fabe
Trini Morrison <TRPMorrison@gmail.com>
2015-06-20 15:30:41 -0400

    DRAFT OF DELETE POSTS AND DELETE ACCOUNT ENTERED INTO MAIN VIEW AND SOLVED VIEWS

--- app/js/views/main.view.js
+++ app/js/views/main.view.js
@@ -15,15 +15,17 @@
     templateSidebar: hbs.sidebar,
 
 
-  initialize: function(options) {
-        var args = options || {};
-        this.collectionUsers = args.collectionUsers;
-        this.collectionPosts = args.collectionPosts;
-        this.collectionUserPosts = args.collectionUserPosts;
-        this.render();
-        $('.container').html(this.el);
+    initialize: function(options) {
+      var args = options || {};
+      this.collectionUsers = args.collectionUsers;
+      this.collectionPosts = args.collectionPosts;
+      this.collectionUserPosts = args.collectionUserPosts;
 
-      },
+      // this.collection('destroy', this.destroyed,this);
+      this.render();
+      $('.container').html(this.el);
+
+    },
 
 
     render: function() {
@@ -32,16 +34,16 @@
       var self = this;
 
       var allUserPosts = $.get(app.rootURL + '/users/' + app.LoggedInUser.username + '/unsolved', function(data) {
-        var response = allUserPosts.responseJSON;
-        console.log(self.$el);
+      var response = allUserPosts.responseJSON;
+      console.log(self.$el);
       }).done(function (data) {
-        self.$el.html(self.template({ image: allUserPosts.responseJSON }));
+      self.$el.html(self.template({ image: allUserPosts.responseJSON }));
       });
 
-      // this.collectionPosts.fetch().done(function (data) {
-      //   this.$el.html(this.template({ image: this.collectionPosts.toJSON() }))
-      // }.bind(this));
+    },
 
+    destroyed: function () {
+      console.log ('Delete your Qpic account?');
     },
 
     // DELETING ACCOUNT
@@ -52,11 +54,13 @@
       var button = event.target;
       var modelID = $(button).data('id');
 
-      // if (window.confirm("Delete your Qpic account?")) {
-      //   this.collection.destroy().success( function () {
-      //      app.mainRouter.navigate('', { trigger: true });
-      //   });
-      //}
+      if (window.confirm("Delete your Qpic account?")) {
+        this.collection.destroy().success(function() {
+        app.mainRouter.navigate('', {
+        trigger: true });
+      });
+    }
+
   }
 
 
