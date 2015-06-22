53cbe10916459e6e89982ea4fd9a648b9b020415
Trini Morrison <TRPMorrison@gmail.com>
2015-06-22 09:01:23 -0400

    SOLUTIONS CODE ADDED - NO CONSOLE ERRORS BUT NOT WORKING

--- app/js/views/solved.view.js
+++ app/js/views/solved.view.js
@@ -1,5 +1,48 @@
+;(function (){
+
+  'use strict';
+
+  app.Views.Solved = Backbone.View.extend({
+
+    className: 'main',
+
+    events: {
+      'click #solves' : 'solves'
+    },
+
+    template: hbs.main,
+    templateSidebar: hbs.sidebar
+
+    initialize: function(options){
+      var args = options || {};
+      this.collectionUsers = args.collectionUsers;
+      this.collectionPosts = args.collectionPosts;
+      this.collectionUserPosts = args.collectionUserPosts;
+
+      this.render();
+
+      $('.container').html(this.el);
+
+    },
+
+    render: function() {
+      var self = this;
+      this.collectionPosts = new app.Collections.Posts();
+
+      var allUserSolves = $.get(app.rootURL + '/posts/:id' + app.LoggedInUser.username, function(data) {
+              var response = allUserSolves.responseJSON;
+            }).done(function (data) {
+              self.$el.html(self.template({ solved: allUserSolves.responseJSON }));
+            });
+
+            $('#solved').html(this.templateSidebar(app.LoggedInUser));
+
+   }
+});
+},
+
 $(function() {
-        $('.deletePostt').on('click', (function(e) {
+        $('.deletePost').on('click', (function(e) {
             e.preventDefault();
 
             var button = button;
